c7704c7668f28da8196d08c7bd898d9f9751ac2b
João Victor <joao5victor5ss@gmail.com>
2021-07-27 15:07:37 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elem = document.querySelectorAll('select');
-  var options = document.querySelectorAll("options");
-  var instances = M.FormSelect.init(elem, options);
-  });
 
   // Or with jQuery
 
@@ -21,29 +16,9 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Modal.init(elems);
 });
 
-// botoes repostas e resultados
-let buttonClick1 = document.getElementById('meubotao1'); //O botao criado deve ter o campo id="meubotao"
-let buttonClick2 = document.getElementById('meubotao2');
-
-console.log(buttonClick1);
-buttonClick1.addEventListener("click",function(event){
-    window.location.href = "../src formulario visualizar/forms_respostas_pelvica.html";
-    console.log("Tentei redirecionar!!!");
-});
-console.log(buttonClick2);
-buttonClick2.addEventListener("click",function(event){
-    window.location.href = "../src formulario concluido/forms_respostas.html";
-    console.log("Tentei redirecionar!!!");
-});
-
-//select de busca
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('select').formSelect();
-  });
+function pesquisa(url){
+    let request = XMLHttpRequest()
+    request.open("GET",url,false);
+    request.send();
+    return request.responseText
+}
